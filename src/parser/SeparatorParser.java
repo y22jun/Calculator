65bf0e3a7fb02569c3ae7f14272d3c9657fadf02
y22jun <tlsdpwns0612@naver.com>
2025-02-15 04:29:49 +0900

    fix:SeparatorParser 클래스에서 두 자릿수 이상의 숫자를 반환 못하는 버그 수정.

--- src/parser/SeparatorParser.java
+++ src/parser/SeparatorParser.java
@@ -1,5 +1,7 @@
 package parser;
 
+import check.CharacterCheck;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,11 +9,30 @@ public class SeparatorParser {
 
     public List<Double> parserSeparator(String input) {
         List<Double> result = new ArrayList<>();
-        for(int i = 0; i < input.length() - 1; i++) {
-            if(Character.isDigit(input.charAt(i))) {
-                result.add(Double.parseDouble(String.valueOf(input.charAt(i))));
-            }
+        StringBuilder number = new StringBuilder();
+
+        for(int i = 0; i < input.length() - 2; i++) {
+            appendNumber(input.charAt(i), number, result);
         }
+
+        addNumberToList(number, result);
         return result;
+    }
+
+    private void appendNumber(char current, StringBuilder number, List<Double> result) {
+        if(!CharacterCheck.isSeparator(current)) {
+            number.append(current);
+            return;
+        }
+        addNumberToList(number, result);
+    }
+
+    private void addNumberToList(StringBuilder number, List<Double> result) {
+        if(number.isEmpty()) {
+            return;
+        }
+
+        result.add(Double.parseDouble(number.toString().trim()));
+        number.setLength(0);
     }
 }
