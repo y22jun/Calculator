86a866875b19bf28d3642af4472ce57291019749
y22jun <tlsdpwns0612@naver.com>
2025-02-15 02:58:05 +0900

    feat:사칙연산에 따른 계산 결과값 출력 기능 구현.

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,8 @@
-import input.InputHandler;
-import operator.AllOperator;
-import parser.OperatorParser;
-import parser.SeparatorParser;
-
-import java.util.List;
+import calculator.Calculator;
 
 public class Main {
     public static void main(String[] args) {
-        InputHandler inputHandler = new InputHandler();
-        String input = inputHandler.getInput();
-        SeparatorParser separatorParser = new SeparatorParser();
-        List<Double> separatorResult = separatorParser.parserSeparator(input);
-        OperatorParser operatorParser = new OperatorParser();
-        char operatorResult = operatorParser.parserOperator(input);
-        AllOperator allOperator = new AllOperator();
-        allOperator.getOperator(operatorResult);
-        System.out.println(separatorResult);
-        System.out.println(operatorResult);
+        Calculator calculator = new Calculator();
+        calculator.calculate();
     }
 }
---
+++ src/calculator/Calculator.java
@@ -0,0 +1,33 @@
+package calculator;
+
+import input.InputHandler;
+import operator.AllOperator;
+import operator.Operator;
+import parser.OperatorParser;
+import parser.SeparatorParser;
+
+import java.util.List;
+
+public class Calculator {
+
+    private final InputHandler inputHandler = new InputHandler();
+    private final SeparatorParser separatorParser = new SeparatorParser();
+    private final OperatorParser operatorParser = new OperatorParser();
+    private final AllOperator allOperator = new AllOperator();
+
+    public void calculate() {
+        String input = inputHandler.getInput();
+        char operator = operatorParser.parserOperator(input);
+        List<Double> numbers = separatorParser.parserSeparator(input);
+        double result = numbers.get(0);
+
+        Operator checkOperator = allOperator.getOperator(operator);
+
+        for(int i = 1; i < numbers.size(); i++) {
+            double currentNumber = numbers.get(i);
+            result = checkOperator.result(result, currentNumber);
+        }
+
+        System.out.println(result);
+    }
+}
