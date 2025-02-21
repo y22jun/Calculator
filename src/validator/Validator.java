8cae0221e644c6a6d287f6c8149e91fa8e80ea6b
y22jun <tlsdpwns0612@naver.com>
2025-02-21 16:28:27 +0900

    Refector:인터페이스 분리를 한 EmptyValidator class를 Validator 안에서 구현.

--- src/validator/Validator.java
+++ src/validator/Validator.java
@@ -1,6 +1,12 @@
 package validator;
 
-public interface Validator {
+import static exception.ErrorMessage.INPUT_EMPTY;
 
-    void validate(String input);
+public class Validator {
+
+    private void NotEmptyValidate(String input) {
+        if(input == null || input.isEmpty()) {
+            throw new IllegalArgumentException(INPUT_EMPTY.getMessage());
+        }
+    }
 }
