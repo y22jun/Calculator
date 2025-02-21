c8d06e7b5cb394ec244df14b789bf2cc2abd4f4b
y22jun <tlsdpwns0612@naver.com>
2025-02-21 16:31:18 +0900

    Refector:인터페이스 분리를 한 ConsecutiveSeparatorValidate class를 Validator 안에서 구현.

--- src/validator/Validator.java
+++ src/validator/Validator.java
@@ -1,12 +1,27 @@
 package validator;
 
+import check.CustomCharacterValidator;
+
+import static exception.ErrorMessage.CONSECUTIVE_SEPARATOR;
 import static exception.ErrorMessage.INPUT_EMPTY;
 
 public class Validator {
 
+    private final CustomCharacterValidator customCharacterValidator = new CustomCharacterValidator();
+
     private void NotEmptyValidate(String input) {
         if(input == null || input.isEmpty()) {
             throw new IllegalArgumentException(INPUT_EMPTY.getMessage());
+        }
+    }
+
+    private void ConsecutiveSeparatorValidate(String input) {
+        for(int i = 0; i < input.length() - 1; i++) {
+            char current = input.charAt(i);
+            char next = input.charAt(i + 1);
+            if(customCharacterValidator.isSeparator(current) && customCharacterValidator.isSeparator(next)) {
+                throw new IllegalArgumentException(CONSECUTIVE_SEPARATOR.getMessage());
+            }
         }
     }
 }
