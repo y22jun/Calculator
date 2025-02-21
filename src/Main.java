96ba41d6ac5b043cf79c192e0bf284a9634eafdd
y22jun <tlsdpwns0612@naver.com>
2025-02-21 15:15:46 +0900

    Chore:Main Class에 있는 변수 이름 변경.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import output.Output;
 
 public class Main {
     public static void main(String[] args) {
-        Output outputHandler = new Output();
-        outputHandler.startCalculator();
+        Output output = new Output();
+        output.startCalculator();
     }
 }
